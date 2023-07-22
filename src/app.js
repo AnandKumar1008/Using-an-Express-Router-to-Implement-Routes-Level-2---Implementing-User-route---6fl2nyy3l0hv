3676dc6c31d1c258a4b06060ddfba1d7fc653e33
Anand Kumar <121444538+AnandKumar1008@users.noreply.github.com>
2023-07-22 16:11:06 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 //Aim:Write a code to Update a product using Patch Request and  to delete a product using DELETE request
 
-const fs = require('fs');
-const express = require('express');
-const { object } = require('joi');
+const fs = require("fs");
+const express = require("express");
+const { object } = require("joi");
 const app = express();
 const router = new express.Router();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 //middleware
 router.use(express.json());
@@ -17,27 +17,83 @@ const product = JSON.parse(
 
 // Defining The Router
 // Handling PATCH request
-router.patch('/api/v1/product/:id', (req, res) => {
+router.patch("/api/v1/product/:id", (req, res) => {
   try {
     //Write your code here
+    const id = req.params.id * 1;
+    let found = false;
+    const modified = product.map((item) => {
+      if (item.id == id) {
+        found = true;
+        return { ...item, ...req.body };
+      }
+      return item;
+    });
+    if (!found) {
+      return res.status(404).json([
+        {
+          message: "Product Not Found",
+
+          status: "Error",
+        },
+      ]);
+    }
+    fs.writeFile(
+      `${__dirname}/..dev-data/product.json`,
+      JSON.stringify(modified),
+      (err) => {
+        res.status(201).json({
+          message: "success",
+
+          data: {
+            product: [...modified],
+          },
+        });
+      }
+    );
   } catch (error) {
     console.log(error);
     res.status(400).json({
-      message: 'Product Updation Failed',
-      status: 'Error',
+      message: "Product Updation Failed",
+      status: "Error",
     });
   }
 });
 
 //Deleting Product
-router.delete('/api/v1/product/:id', (req, res) => {
+router.delete("/api/v1/product/:id", (req, res) => {
   try {
     //Write your code here
+    const id = req.params.id;
+    const filteredItem = product.filter((item) => item.id != id);
+    if (filteredItem.length == product.length)
+      return res.status(400).json([
+        {
+          message: "Product Deletion Failed",
+
+          status: "Error",
+        },
+      ]);
+    else {
+      fs.writeFile(
+        `${__dirname}/../dev-data/product.json`,
+        JSON.stringify(filteredItem),
+        (err) => {
+          res.status(201).json({
+            status: "success",
+
+            data: {
+              product: [...filteredItem],
+            },
+          });
+        }
+      );
+    }
   } catch (error) {
     console.log(error);
     res.status(400).json({
-      message: 'Product Deletion Failed',
-      status: 'Error',
+      message: "Product Deletion Failed",
+      status: "Error",
     });
   }
 });
